6ebeed0e50cb2bfb18206ae07f29dbb5d9c84829
datungLA <97591324+datungLA@users.noreply.github.com>
2023-10-25 15:28:05 -0700

    testing arrayExamples

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,18 +1,39 @@
 import static org.junit.Assert.*;
+
+import java.lang.reflect.Array;
+
 import org.junit.*;
 
 public class ArrayTests {
-	@Test 
-	public void testReverseInPlace() {
+  @Test
+  public void testReverseInPlaceWithOneElement() {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
-	}
-
+    assertArrayEquals(new int[] { 3 }, input1);
+  }
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = {};
+    assertArrayEquals(new int[] {}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReverseInPlaceWithMoreElements() {
+    int[] input1 = { 1, 2, 3, 4, 5, 6 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[] { 6, 5, 4, 3, 2, 1 }, input1);
+  }
+
+  @Test
+  public void testReversedWithElements() {
+    int[] input1 = { 1, 2, 3 };
+    assertArrayEquals(new int[] { 3, 2, 1 }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReverseArrAddress() {
+    int[] input1 = { 1, 2, 3 };
+    assertNotSame(input1, ArrayExamples.reversed(input1));
   }
 }
