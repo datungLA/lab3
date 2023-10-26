fa012b542d64141ad08ba9b4ab9c36cf719476b9
datungLA <97591324+datungLA@users.noreply.github.com>
2023-10-26 05:36:34 -0700

    createdLinkedListTestsFile

---
+++ LinkedListTests.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.*;
+
+public class LinkedListTests {
+    @Test
+    public void fun() {
+
+    }
+}
